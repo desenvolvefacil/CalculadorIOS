8de958ed268b7cd55be0f2dec42f95f60680829a
Desenvolvefacil <contato@desenvolvefacil.com.br>
2018-10-25 20:35:23 -0300

    asdas

--- CalculadorIOS/ViewController.swift
+++ CalculadorIOS/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ResultadoLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +22,12 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
 
-
+    @IBAction func Click_7() {
+        ResultadoLabel.text = "7";
+    }
+    
 }
 
